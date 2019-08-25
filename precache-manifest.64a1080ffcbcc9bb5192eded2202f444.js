4f3de3bb48e456b13225c26639ffeb3934d1484b
David Garza <dgarza0413@gmail.com>
2019-08-25 18:54:58 -0500

    Updates

---
+++ precache-manifest.64a1080ffcbcc9bb5192eded2202f444.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4cd6a798a8f5a44a882403f05acca16",
+    "url": "/tetris-react/index.html"
+  },
+  {
+    "revision": "65a3b4ac617e1c27e7d9",
+    "url": "/tetris-react/static/css/main.6f03ce00.chunk.css"
+  },
+  {
+    "revision": "ee09f2619323820bd686",
+    "url": "/tetris-react/static/js/2.ed3f757a.chunk.js"
+  },
+  {
+    "revision": "65a3b4ac617e1c27e7d9",
+    "url": "/tetris-react/static/js/main.935c2271.chunk.js"
+  },
+  {
+    "revision": "5a1211c0f695df8a0b3f",
+    "url": "/tetris-react/static/js/runtime~main.5b09f388.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/tetris-react/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/tetris-react/static/media/bg.2810fe6b.png"
+  }
+]);
